1a422441d9a937d56243e97374d82f5675c66b05
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2020-11-25 14:22:48 +0100

    feat: test with CORS
    ```
    $ node server.js
    $ node server2.js
    // and then open http://localhost:3000 in a browser (which will target the Socket.IO server on port 3001)
    ```

---
+++ server2.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+const port = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', socket => {
+  console.log(`connect ${socket.id}`);
+
+  socket.emit('hello', 1, '2', {
+    hello: 'you'
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`disconnect ${socket.id}`);
+  });
+});
+
+server.listen(port, () => console.log(`server listening on port ${port}`));
